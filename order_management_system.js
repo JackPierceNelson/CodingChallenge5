62e1abee37179296e7e7bcb9fbae9283df9dba0b
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-09-20 11:34:01 -0500

    Create placeOrder function

--- order_management_system.js
+++ order_management_system.js
@@ -17,3 +17,34 @@ orders.push(
     {customerName: "Robert", 
         items: [{name: "Latte", quantity: 1}], status: "Complete"}
 )
+
+// Task 3: Create a Function to Place an Order
+
+function placeOrder(customerName, orderedItems) {
+    // Check if there are enough stock for each item in the order
+    for(let item of orderedItems) {const inventoryItem = Inventory.find(product => product.name === item.name);
+       if (!inventoryItem) {
+        console.error(`Error: ${item.name} is not found in inventory`);
+        return;
+       } 
+       if (inventoryItem.quantity < item.quantity) {
+        console.error(`Error: Insufficient stock for ${item.name}. Available: ${inventoryItem.quantity}, Ordered: ${item.quantity}`);
+        return;
+       }
+
+    }
+    // If all items are available, subtract the ordered quantity from the products stock
+    for (let item of orderedItems) {const inventoryItem = Inventory.find(product => product.name === item.name);
+        inventoryItem.quantity -= item.quantity
+        
+    }
+    // Add a new order in the order array
+    const newOrder = {
+        customerName: customerName, items: orderedItems, status: "Pending"
+    };
+    orders.push(newOrder);
+    console.log(`Order placed succesfully for ${customerName}`);
+}
+placeOrder("Alice", [{
+    name: "Latte", quantity: 1}
+]);
